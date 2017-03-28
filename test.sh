89eab92b531eb4a8cd3cce1e9696f13e434d4d77
maciej-stanek <makaron28@gmail.com>
2017-03-28 18:35:27 +0200

    Imagemagick bug description added

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 points="100,100 200,200 300,200 400,300 400,400 500,500"
 cb=#45961b
 cf=#67bf39
+# http://www.imagemagick.org/discourse-server/viewtopic.php?t=26666
 
 convert -size 600x600 \
 	xc:white \

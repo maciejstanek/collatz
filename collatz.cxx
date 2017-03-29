dc560f00e832a401e007824d2991b8704366252c
maciej-stanek <makaron28@gmail.com>
2017-03-29 09:28:38 +0200

    wip

--- collatz.cxx
+++ collatz.cxx
@@ -16,21 +16,26 @@ class Vect {
 		}
 		Vect *getNewNext(double step, double angleChange) {
 			return new Vect(
-				x + step*sin(a+angleChange),
-				y + step*sin(a+angleChange),
+				x + step*sin(a),
+				y + step*cos(a),
 				a + angleChange);
 		}
 };
 
 int main(int argc, char *argv[]) {
-	if(argc != 2) {
+	if(argc != 4) {
 		printf("Collatz Conjecture Visualizer\n");
-		printf("Usage: ./collatz <limit>\n");
-		printf(" <limit> - the top number to visualize\n");
+		printf("Usage: ./collatz <limit> <angle> <step>\n");
+		printf(" | <limit> - the top number to visualize\n");
+		printf(" | <angle> - angle per each iteration\n");
+		printf(" | <step>  - step per each iteration");
 		return 1;
 	}
-
+	
+	
 	int limit = atoi(argv[1]);
+	double angleChange = atof(argv[2]);
+	double step = atof(argv[3]);
 
 	vector<int> jumps(limit+1, 0);
 	for(vector<int>::iterator i = jumps.begin(); i != jumps.end(); i++) {
@@ -79,28 +84,42 @@ int main(int argc, char *argv[]) {
 	vector<vector<Vect*>*> physicalPaths;
 
 	for(vector<vector<int>*>::reverse_iterator i = paths.rbegin(); i != paths.rend(); i++) {
-		for(vector<int>::reverse_iterator j = (*i)->rbegin(); j != (*i)->rend(); j++) {
+		physicalPaths.push_back(new vector<Vect*>);
+		physicalPaths.back()->push_back(new Vect(0, 0, M_PI));
+		for(vector<int>::reverse_iterator j = (*i)->rbegin() + 1; j != (*i)->rend(); j++) {
 			printf("%d ", *j);
+			Vect *newVect;
+			newVect = physicalPaths.back()->back()->getNewNext(step, ((*j)%2?-1:1)*angleChange);
+			physicalPaths.back()->push_back(newVect);
 		}
 		printf("\n");
 	}
-		/*
-		vector<> *list = new vector<int>;
-		int next = index;
-		list->push_back(next);
-		while(jumps[next] >= 0) {
-			#ifdef DEBUG
-			printf("[%d][%d]\n", next, jumps[next]);
-			#endif
-			next = jumps[next];
-			list->push_back(next);
+	int imgSize = 2200;
+	FILE *fp;
+	fp = fopen("cmd.sh", "w");
+	fprintf(fp, "#!/bin/bash\n");
+	fprintf(fp, "c1=#45961b\n");
+	fprintf(fp, "c2=#67bf39\n");
+	fprintf(fp, "convert -size %dx%d xc:white \\\n", imgSize, imgSize);
+
+	for(vector<vector<Vect*>*>::iterator i = physicalPaths.begin(); i != physicalPaths.end(); i++) {
+		fprintf(fp, "\t-fill none -stroke black -draw \"polyline");
+		for(vector<Vect*>::iterator j = (*i)->begin(); j != (*i)->end(); j++) {
+			fprintf(fp, " %d,%d", imgSize/2+(int)round((*j)->x), imgSize/2+(int)round((*j)->y));
 		}
-		#ifdef DEBUG
-		printf("\n");
-		#endif
-		paths.push_back(list);
-		*/
-	//}
+		fprintf(fp, "\" \\\n");
+	}
+	fprintf(fp, "\tout.png");
+	fclose(fp);
+
+	//-fill none -stroke $c1 -strokewidth 20 -draw "stroke-linecap round bezier $points" \
+	out.png
+	//-fill $c1 -stroke none -draw "circle 500,500 510,500" \
+	//-fill none -stroke $c2 -strokewidth 16 -draw "stroke-linecap round bezier $points" \
+	//-fill $c2 -stroke none -draw "circle 500,500 508,500" \
+
+	system("chmod +x cmd.sh");
+	system("./cmd.sh");
 
 	return 0;
 }
